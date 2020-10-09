7df9525ebfa53455c482091e56a67acabf38774f
Pierre1305 <pierrefarah77@gmail.com>
2020-10-09 13:04:44 +0200

    Klart
    Klart

---
+++ Uppgift3/Klasser/Personer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klasser
+{
+    class Personer
+    {
+        public string Namn { get; set; }
+        public int Alder { get; set; }
+        private List<Bil> _bilar;
+        public List<Bil> Bilar
+        {
+            get
+            {
+                if (_bilar == null)
+                {
+                    _bilar = new List<Bil>();
+                }
+                return _bilar;
+            }
+        }
+    }
+}
