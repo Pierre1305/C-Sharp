2b9628c80428b3280bc6eb5af65edfafae3e697c
Pierre1305 <pierrefarah77@gmail.com>
2020-10-09 12:38:45 +0200

    Klart
    Klart

---
+++ Uppgift3/Klasser/Bil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klasser
+{
+    class Bil
+    {
+        public string ModelNamn;
+        private decimal _milMatare;
+
+        public Bil(string modellnamn)
+        {
+            this.ModelNamn = modellnamn;
+        }
+
+        public string RegistreringsNummer { get; set; }
+        public int ViktIKilo { get; set; }
+        public DateTime RegistreringsDatum { get; set; }
+        public bool Elbil { get; set; }
+
+        public void KorMil(decimal mil)
+        {
+            if (mil > 0)
+            {
+                this._milMatare += mil;
+            }
+        }
+
+        public string GetMil()
+        {
+            return this._milMatare.ToString();
+        }
+    }
+}
